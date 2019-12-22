a804a29cbc182249ca26b41df2a232c1c769e044
nangkossam <kr.dotware@gmail.com>
2019-12-22 22:23:48 +0900

    drawer 메뉴 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-toolbar flat color="transparent">
+        <v-toolbar-title>Account</v-toolbar-title>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-tile v-for="item in items" :key="item.title" :to="item.to">
+          <v-list-tile-avatar>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon @click="toggleDrawer"></v-toolbar-side-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
       <v-spacer></v-spacer>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+
     </v-toolbar>
 
     <v-content>
@@ -29,7 +41,28 @@ export default {
   name: 'App',
   data () {
     return {
-      //
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-alert',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'mdi-alert-box',
+          title: 'About',
+          to: '/about'
+        },
+        {
+          icon: 'mdi-alert-circle',
+          title: 'About2'
+        }
+      ]
+    }
+  },
+  methods: {
+    toggleDrawer () {
+      this.drawer = !this.drawer
     }
   }
 }
